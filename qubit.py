4a87410cbca45ae3744f793dd33c9730b6f9e66e
Neil Dick <a-nedick@microsoft.com>
2019-06-13 09:13:03 +1000

    Add H gate
    Add the Hadamard gate to the qubit class.

--- qubit.py
+++ qubit.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 
 one = np.array([[0], [1]])
 zero = np.array([[1], [0]])
@@ -8,6 +9,12 @@ class Qubit:
         self.a = a
         self.b = b
         self.state = self.a*zero + self.b*one
+
+
+    def H(self):
+        self.state = np.dot((1/sqrt(2)) * np.array([[1,  1],
+                                                    [1, -1]]),
+                            self.state)
 
     def X(self):
         self.state = np.dot(np.array([[0, 1],
