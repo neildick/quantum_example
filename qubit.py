d60efc07fdc01070f99040a217d8f0c6edc6192f
Neil Dick <a-nedick@microsoft.com>
2019-06-13 09:05:45 +1000

    Initial Qubit Class
    Initial framework for the qubit

---
+++ qubit.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+one = np.array([[0], [1]])
+zero = np.array([[1], [0]])
+
+class Qubit:
+    def __init__(self, a=1.0, b=0.0):
+        self.a = a
+        self.b = b
+        self.state = self.a*zero + self.b*one
